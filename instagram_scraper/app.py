1e9fa8b209b524e85846a9c64c45a159f5b245b9
rarcega <rarcega@users.noreply.github.com>
2017-07-01 15:53:01 -0400

    Adds verify account handling for login failure #69

--- instagram_scraper/app.py
+++ instagram_scraper/app.py
@@ -74,9 +74,13 @@ class InstagramScraper(object):
         else:
             self.logger.error('Login failed for ' + self.login_user)
 
-            for count, error in enumerate(login_text.get('errors').get('error')):
-                count += 1
-                self.logger.debug('Session error %(count)s: "%(error)s"' % locals())
+            if 'challenge_url' in login_text:
+                self.logger.error('Login failed for {0}. Please verify your account at {1}'
+                                  .format(self.login_user, BASE_URL + login_text.get('checkpoint_url')))
+            else:
+                for count, error in enumerate(login_text.get('errors').get('error')):
+                    count += 1
+                    self.logger.debug('Session error %(count)s: "%(error)s"' % locals())
 
     def logout(self):
         """Logs out of instagram."""
